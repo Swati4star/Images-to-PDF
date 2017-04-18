ee2203be56153ff44e774cf13d89c24a4c07fc4b
swati4star <swati4star@gmail.com>
2017-04-18 19:52:41 +0530

    removed circle.yaml : removing CircleCI support; useless #9
    Updated build tools and graddle
    
    Created AsyncTask for populating the list of pdfs
    
    Revert graddle to 2.3.0
    
    Updated graddle and libraries
    
    Added line so travis-ci works
    
    Updated build tools in travis file
    
    Added strings to the ja string file as well
    
    conflicts resolved
    
    travis fix
    
    update build version

--- app/src/main/java/swati4star/createpdf/ViewFiles.java
+++ app/src/main/java/swati4star/createpdf/ViewFiles.java
@@ -2,6 +2,7 @@ package swati4star.createpdf;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.Fragment;
@@ -13,11 +14,10 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+
 import java.io.File;
 import java.util.ArrayList;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class ViewFiles extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
@@ -59,7 +59,7 @@ public class ViewFiles extends Fragment implements SwipeRefreshLayout.OnRefreshL
         swipeView.setOnRefreshListener(this);
 
         // Populate data into listView
-        populateListView();
+        new PopulateList().execute();
 
         return root;
     }
@@ -76,24 +76,60 @@ public class ViewFiles extends Fragment implements SwipeRefreshLayout.OnRefreshL
         else {
             for (File file : files) {
                 if (!file.isDirectory() && file.getName().endsWith(".pdf")) {
-                        inFiles.add(file.getPath());
-                        Log.v("adding", file.getName());
-
+                    inFiles.add(file.getPath());
+                    Log.v("adding", file.getName());
                 }
             }
-
         }
         Log.v("done", "adding");
         adapter = new FilesAdapter(activity, inFiles);
         listView.setAdapter(adapter);
-
     }
 
     @Override
     public void onRefresh() {
 
         Log.v("refresh", "refreshing dta");
-        populateListView();
+        new PopulateList().execute();
         swipeView.setRefreshing(false);
+    }
+
+    /**
+     * AsyncTask used to populate the list of elements in the background
+     */
+    public class PopulateList extends AsyncTask<Void, Void, Void> {
+
+        // Progress dialog
+        MaterialDialog.Builder builder = new MaterialDialog.Builder(activity)
+                .title(getActivity().getResources().getString(R.string.please_wait))
+                .content(getActivity().getResources().getString(R.string.populating_list))
+                .cancelable(false)
+                .progress(true, 0);
+        MaterialDialog dialog = builder.build();
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            getActivity().runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    populateListView();
+                }
+            });
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void v) {
+            super.onPostExecute(v);
+            dialog.dismiss();
+        }
     }
 }
