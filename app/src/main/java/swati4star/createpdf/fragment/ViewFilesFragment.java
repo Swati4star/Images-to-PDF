3ca3d237512855b3eaf3341924d1f40bbb808fd9
sabihashaik <sabihaabc1@gmail.com>
2018-06-24 10:09:10 +0400

    UI Enhancement for No PDF to Show Screen
    UI Enhancement Image
    
    UI Screens for No PDF Review
    
    UI Screens for No PDF Review
    
    UI Screens for No PDF Review 3

--- app/src/main/java/swati4star/createpdf/fragment/ViewFilesFragment.java
+++ app/src/main/java/swati4star/createpdf/fragment/ViewFilesFragment.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -22,6 +21,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -50,8 +50,14 @@ public class ViewFilesFragment extends Fragment
     RecyclerView mViewFilesListRecyclerView;
     @BindView(R.id.swipe)
     SwipeRefreshLayout mSwipeView;
-    @BindView(R.id.emptyStatusTextView)
-    public TextView emptyStatusTextView;
+    @BindView(R.id.emptyBackgroundImage)
+    public ImageView backView;
+    @BindView(R.id.emptyTextOverBgImage)
+    public TextView TextOver;
+    @BindView(R.id.getStarted)
+    public TextView getStarted;
+    @BindView(R.id.emptyTagLine)
+    public TextView tagLine;
     private int mCurrentSortingIndex = -1;
     private FileUtils mFileUtils;
 
@@ -83,7 +89,7 @@ public class ViewFilesFragment extends Fragment
         final ArrayList<File> pdfFiles = new ArrayList<>();
         final File[] files = folder.listFiles();
         if (files.length == 0) {
-            emptyStatusTextView.setVisibility(View.VISIBLE);
+            setEmptyStateVisible();
         }
         mViewFilesAdapter = new ViewFilesAdapter(mActivity, pdfFiles, this);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(root.getContext());
@@ -198,12 +204,22 @@ public class ViewFilesFragment extends Fragment
 
     @Override
     public void setEmptyStateVisible() {
-        emptyStatusTextView.setVisibility(View.VISIBLE);
+
+        backView.setVisibility(View.VISIBLE);
+        TextOver.setVisibility(View.VISIBLE);
+        getStarted.setVisibility(View.VISIBLE);
+        tagLine.setVisibility(View.VISIBLE);
+
     }
 
     @Override
     public void setEmptyStateGone() {
-        emptyStatusTextView.setVisibility(View.GONE);
+
+        backView.setVisibility(View.GONE);
+        TextOver.setVisibility(View.GONE);
+        getStarted.setVisibility(View.GONE);
+        tagLine.setVisibility(View.GONE);
+
     }
 
     /**
