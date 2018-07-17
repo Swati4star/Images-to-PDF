b6868ce840da61d600258c746b63dfaf419b77ac
shiv07tiwari <shivansh16tiwari@gmail.com>
2018-07-17 17:14:08 +0530

    Enhancement options in text to pdf

--- app/src/main/java/swati4star/createpdf/fragment/TextToPdfFragment.java
+++ app/src/main/java/swati4star/createpdf/fragment/TextToPdfFragment.java
@@ -9,6 +9,8 @@ import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,24 +22,33 @@ import com.itextpdf.text.DocumentException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import swati4star.createpdf.R;
+import swati4star.createpdf.adapter.EnhancementOptionsAdapter;
+import swati4star.createpdf.util.EnhancementOptionsEntity;
 import swati4star.createpdf.util.FileUtils;
 import swati4star.createpdf.util.PDFUtils;
 import swati4star.createpdf.util.StringUtils;
 
 import static android.app.Activity.RESULT_OK;
 
-public class TextToPdfFragment extends Fragment {
+public class TextToPdfFragment extends Fragment implements EnhancementOptionsAdapter.OnItemClickListner  {
 
     private final int mFileSelectCode = 0;
+    private final ArrayList<EnhancementOptionsEntity> mTextEnhancementOptionsEntityArrayList = new ArrayList<>();
     @BindView(R.id.tv_file_name)
     TextView mTextView;
     private Activity mActivity;
     private Uri mTextFileUri = null;
+    private int mFontSize;
+    @BindView(R.id.enhancement_options_recycle_view_text)
+    RecyclerView mTextEnhancementOptionsRecycleView;
+    private EnhancementOptionsAdapter mTextEnhancementOptionsAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,7 +60,7 @@ public class TextToPdfFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_text_to_pdf, container, false);
         ButterKnife.bind(this, rootview);
-
+        showEnhancementOptions();
         Button selectButton = rootview.findViewById(R.id.selectFile);
         selectButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,7 +81,61 @@ public class TextToPdfFragment extends Fragment {
         });
         return rootview;
     }
+    public List<EnhancementOptionsEntity> getEnhancementOptions() {
+        mTextEnhancementOptionsEntityArrayList.clear();
 
+        mTextEnhancementOptionsEntityArrayList.add(
+                new EnhancementOptionsEntity(getResources().getDrawable(R.drawable.ic_font_black_24dp),
+                        getResources().getString(R.string.edit_font_size)));
+        return mTextEnhancementOptionsEntityArrayList;
+    }
+    private void showEnhancementOptions() {
+        GridLayoutManager mGridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        mTextEnhancementOptionsRecycleView.setLayoutManager(mGridLayoutManager);
+        mTextEnhancementOptionsAdapter = new EnhancementOptionsAdapter(this, getEnhancementOptions());
+        mTextEnhancementOptionsRecycleView.setAdapter(mTextEnhancementOptionsAdapter);
+    }
+    @Override
+    public void onItemClick(int position) {
+        switch (position) {
+            case 0:
+                editFontSize();
+                break;
+        }
+    }
+    private void editFontSize() {
+        new MaterialDialog.Builder(mActivity)
+                .title(R.string.edit_font_size)
+                .content(R.string.enter_font_size)
+                .input(getString(R.string.example_font), null, new MaterialDialog.InputCallback() {
+                    @Override
+                    public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
+                        try {
+                            mFontSize = Integer.parseInt(String.valueOf(input));
+                            if (mFontSize > 1000 || mFontSize < 0) {
+                                Snackbar.make(Objects.requireNonNull(mActivity).findViewById(android.R.id.content),
+                                        R.string.invalid_quality,
+                                        Snackbar.LENGTH_LONG).show();
+                            } else {
+                                showFontSize();
+                                Snackbar.make(Objects.requireNonNull(mActivity).findViewById(android.R.id.content),
+                                        R.string.font_size_changed,
+                                        Snackbar.LENGTH_LONG).show();
+                            }
+                        } catch (NumberFormatException e) {
+                            Snackbar.make(Objects.requireNonNull(mActivity).findViewById(android.R.id.content),
+                                    R.string.invalid_quality,
+                                    Snackbar.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .show();
+    }
+    private void showFontSize() {
+        mTextEnhancementOptionsEntityArrayList.get(0)
+                .setName("Font Size : " + mFontSize);
+        mTextEnhancementOptionsAdapter.notifyDataSetChanged();
+    }
     public void openCreateTextPdf() {
         new MaterialDialog.Builder(mActivity)
                 .title(R.string.creating_pdf)
@@ -89,7 +154,7 @@ public class TextToPdfFragment extends Fragment {
                             mPath = mPath + mFilename + mActivity.getString(R.string.pdf_ext);
                             try {
                                 PDFUtils fileUtil = new PDFUtils(mActivity);
-                                fileUtil.createPdf(mTextFileUri, mFilename);
+                                fileUtil.createPdf(mTextFileUri, mFilename, mFontSize);
                                 final String finalMPath = mPath;
                                 Snackbar.make(Objects.requireNonNull(mActivity).findViewById(android.R.id.content)
                                         , R.string.snackbar_pdfCreated
