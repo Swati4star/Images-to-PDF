8ef83fdb48e5e9c91f776c23442a54956bc897fa
vyankatesh <vyankateshberde24@gmail.com>
2018-07-23 23:42:28 +0530

    fixes for #263 added new feature "Set Font Family"

--- app/src/main/java/swati4star/createpdf/model/TextToPDFOptions.java
+++ app/src/main/java/swati4star/createpdf/model/TextToPDFOptions.java
@@ -1,15 +1,18 @@
 package swati4star.createpdf.model;
 
 import android.net.Uri;
+import com.itextpdf.text.Font;
 
 public class TextToPDFOptions extends PDFOptions {
     private final Uri mInFileUri;
     private final int mFontSize;
+    private final Font.FontFamily mFontFamily;
 
-    public TextToPDFOptions(String mFileName, Uri mInFileUri, int mFontSize) {
+    public TextToPDFOptions(String mFileName, Uri mInFileUri, int mFontSize, Font.FontFamily mFontFamily) {
         super(mFileName);
         this.mInFileUri = mInFileUri;
         this.mFontSize = mFontSize;
+        this.mFontFamily = mFontFamily;
     }
 
     public Uri getInFileUri() {
@@ -18,5 +21,9 @@ public class TextToPDFOptions extends PDFOptions {
 
     public int getFontSize() {
         return mFontSize;
+    }
+
+    public Font.FontFamily getFontFamily() {
+        return mFontFamily;
     }
 }
