b086618855175a340776bbc150eb21011508a833
Abel Joseph John <eddy.gta17@gmail.com>
2018-07-26 20:55:43 +0530

    #218 - Adding back button & cancel option filter screen (#279)

--- app/src/main/java/swati4star/createpdf/activity/PreviewActivity.java
+++ app/src/main/java/swati4star/createpdf/activity/PreviewActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.eftimoff.viewpagertransformers.DepthPageTransformer;
 
