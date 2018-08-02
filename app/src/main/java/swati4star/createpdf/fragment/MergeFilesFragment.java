7e95de6ebd2e8e43b154f8a6c8153b1a0a0a802e
vyankatesh <vyankateshberde24@gmail.com>
2018-08-03 00:30:31 +0530

    #277 View Files in Merge pdf section.

--- app/src/main/java/swati4star/createpdf/fragment/MergeFilesFragment.java
+++ app/src/main/java/swati4star/createpdf/fragment/MergeFilesFragment.java
@@ -10,13 +10,17 @@ import android.os.Environment;
 import android.provider.OpenableColumns;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -26,16 +30,21 @@ import com.itextpdf.text.pdf.PdfReader;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import swati4star.createpdf.R;
+import swati4star.createpdf.adapter.MergeFilesAdapter;
+import swati4star.createpdf.util.DirectoryUtils;
 import swati4star.createpdf.util.StringUtils;
+import swati4star.createpdf.util.ViewFilesDividerItemDecoration;
 
 import static android.app.Activity.RESULT_OK;
-public class MergeFilesFragment extends Fragment {
+
+public class MergeFilesFragment extends Fragment implements MergeFilesAdapter.OnClickListener {
     private Activity mActivity;
     private boolean mSuccess;
     private String mFilename;
@@ -44,6 +53,9 @@ public class MergeFilesFragment extends Fragment {
     private String mRetfoldername;
     private String mRealPath;
     private String mDisplayName;
+    private MergeFilesAdapter mMergeFilesAdapter;
+    private DirectoryUtils mDirectoryUtils;
+    private ArrayList<String> mAllFilesPaths;
     @BindView(R.id.textView)
     TextView nosupport;
     @BindView(R.id.fileonebtn)
@@ -52,8 +64,19 @@ public class MergeFilesFragment extends Fragment {
     Button addFileTwo;
     @BindView(R.id.mergebtn)
     Button mergeBtn;
+    @BindView(R.id.recyclerViewFiles)
+    RecyclerView mRecyclerViewFiles;
+    @BindView(R.id.viewFiles)
+    TextView mViewFiles;
+    @BindView(R.id.tableLayout)
+    ConstraintLayout mTableLayout;
+    @BindView(R.id.upArrow)
+    ImageView mUpArrow;
+    @BindView(R.id.downArrow)
+    ImageView mDownArrow;
     String firstFilePath;
     String secondFilePath;
+    private boolean mFilesShowing = true;
 
 
     public MergeFilesFragment() {
@@ -65,7 +88,36 @@ public class MergeFilesFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_merge_files, container, false);
         ButterKnife.bind(this, root);
+        mDirectoryUtils = new DirectoryUtils(mActivity);
+        mAllFilesPaths = getAllFilePaths();
+        mMergeFilesAdapter = new MergeFilesAdapter(mActivity, mAllFilesPaths, this);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(mActivity);
+        mRecyclerViewFiles.setLayoutManager(mLayoutManager);
+        mRecyclerViewFiles.setAdapter(mMergeFilesAdapter);
+        mRecyclerViewFiles.addItemDecoration(new ViewFilesDividerItemDecoration(mActivity));
+
         return root;
+    }
+
+    @OnClick(R.id.viewFiles)
+    void onViewFilesClick(View view) {
+        if (mFilesShowing) {
+            addFileOne.setVisibility(View.VISIBLE);
+            addFileTwo.setVisibility(View.VISIBLE);
+            mergeBtn.setVisibility(View.VISIBLE);
+            mRecyclerViewFiles.setVisibility(View.GONE);
+            mUpArrow.setVisibility(View.VISIBLE);
+            mDownArrow.setVisibility(View.GONE);
+            mFilesShowing = !mFilesShowing;
+        } else {
+            addFileOne.setVisibility(View.GONE);
+            addFileTwo.setVisibility(View.GONE);
+            mergeBtn.setVisibility(View.GONE);
+            mRecyclerViewFiles.setVisibility(View.VISIBLE);
+            mUpArrow.setVisibility(View.GONE);
+            mDownArrow.setVisibility(View.VISIBLE);
+            mFilesShowing = !mFilesShowing;
+        }
     }
 
     @OnClick(R.id.fileonebtn)
@@ -137,17 +189,17 @@ public class MergeFilesFragment extends Fragment {
     private void showFileChooser() {
         String folderPath = Environment.getExternalStorageDirectory() + "/";
         Intent intent = new Intent();
-        intent.setAction( Intent.ACTION_GET_CONTENT);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
         Uri myUri = Uri.parse(folderPath);
-        intent.setDataAndType( myUri , getString(R.string.pdf_type));
-        Intent intentChooser = Intent.createChooser(intent , getString(R.string.merge_file_select));
-        startActivityForResult(intentChooser , INTENT_REQUEST_PICKFILE_CODE);
+        intent.setDataAndType(myUri, getString(R.string.pdf_type));
+        Intent intentChooser = Intent.createChooser(intent, getString(R.string.merge_file_select));
+        startActivityForResult(intentChooser, INTENT_REQUEST_PICKFILE_CODE);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) throws NullPointerException {
         if (data != null) {
 
-            if (requestCode == INTENT_REQUEST_PICKFILE_CODE ) {
+            if (requestCode == INTENT_REQUEST_PICKFILE_CODE) {
                 if (resultCode == RESULT_OK) {
                     Uri uri = data.getData();
                     String uriString = uri.toString();
@@ -169,6 +221,7 @@ public class MergeFilesFragment extends Fragment {
             }
         }
     }
+
     //Returns the complete filepath of the PDF as a string
     private String getFilePath(String uriString, Uri uri, File myFile, String path) {
         String filepath = null;
@@ -188,7 +241,7 @@ public class MergeFilesFragment extends Fragment {
         return filepath;
     }
 
-    private String  getParentFolder(String p) {
+    private String getParentFolder(String p) {
         try {
             //Get Name of Parent Folder of File
             // Folder Name found between first occurance of string %3A and %2F from path
@@ -221,6 +274,7 @@ public class MergeFilesFragment extends Fragment {
         }
         return mDisplayName;
     }
+
     //Returns the folder and file name as string
     private String setPathontextview(String folname) {
         if (folname != null) {
@@ -239,7 +293,7 @@ public class MergeFilesFragment extends Fragment {
     }
 
     @Override
-    public void onSaveInstanceState(@NonNull  Bundle outState) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(getString(R.string.btn_sav_text), mCheckbtClickTag);
     }
@@ -253,5 +307,49 @@ public class MergeFilesFragment extends Fragment {
     private void showSnackbar(int resID) {
         Snackbar.make(Objects.requireNonNull(mActivity).findViewById(android.R.id.content),
                 resID, Snackbar.LENGTH_LONG).show();
+    }
+
+    private ArrayList<String> getAllFilePaths() {
+        ArrayList<String> pdfPaths = new ArrayList<>();
+        ArrayList<File> pdfFiles;
+        ArrayList<File> pdfFromOtherDir = mDirectoryUtils.getPdfFromOtherDirectories();
+        final File[] files = mDirectoryUtils.getOrCreatePdfDirectory().listFiles();
+        if ((files == null || files.length == 0) && pdfFromOtherDir == null) {
+            return null;
+        } else {
+
+            pdfFiles = mDirectoryUtils.getPdfsFromPdfFolder(files);
+            if (pdfFromOtherDir != null) {
+                pdfFiles.addAll(pdfFromOtherDir);
+            }
+        }
+        if (pdfFiles != null) {
+            for (File pdf : pdfFiles) {
+                pdfPaths.add(pdf.getAbsolutePath());
+            }
+        }
+        return pdfPaths;
+    }
+
+    @Override
+    public void onItemClick(String path) {
+        new MaterialDialog.Builder(mActivity)
+                .title(R.string.select_as)
+                .items(R.array.select_as_options)
+                .itemsCallback((dialog, itemView, position, text) -> {
+                    switch (position) {
+                        case 0:
+                            firstFilePath = path;
+                            addFileOne.setText(path);
+                            break;
+                        case 1:
+                            addFileTwo.setText(path);
+                            secondFilePath = path;
+                            mSuccess = true;
+                            break;
+                    }
+                })
+                .show();
+
     }
 }
