735d416b74f7fc24e211772e387341b848d0b403
Swati4star <swati4star@gmail.com>
2018-09-15 14:08:38 +0530

    added more options

--- app/src/main/java/swati4star/createpdf/customviews/MyCardView.java
+++ app/src/main/java/swati4star/createpdf/customviews/MyCardView.java
@@ -17,27 +17,49 @@ public class MyCardView extends LinearLayout {
     ImageView icon;
     TextView text;
 
+    /**
+     * Initiates custom card view
+     * @param context - context
+     */
     public MyCardView(@NonNull Context context) {
         super(context);
         init();
     }
 
+    /**
+     * Initiates custom card view with attributes
+     * @param context - context
+     * @param attrs - attributes set
+     */
     public MyCardView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(attrs);
     }
 
+    /**
+     * Initiates custom card view with attributes set
+     * @param context - context
+     * @param attrs - attributes set
+     * @param defStyleAttr - attrbute style
+     */
     public MyCardView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(attrs);
     }
 
+    /**
+     * Initialize cardview with no attribute sets
+     */
     private void init() {
         inflate(getContext(), R.layout.item_view_enhancement_option, this);
         this.text = findViewById(R.id.option_name);
         this.icon = findViewById(R.id.option_image);
     }
 
+    /**
+     * Initialize cardview with attributes recevied
+     * @param attrs - attribute set
+     */
     private void init(AttributeSet attrs) {
         inflate(getContext(), R.layout.item_view_enhancement_option, this);
 
