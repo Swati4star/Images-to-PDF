57d8a184c67559b2113a9f46d4db154905177c55
Mahima <mahima.6.ms@gmail.com>
2018-10-15 12:38:27 +0530

    PDFToImage: set up bottom sheet data
    PDFToImage: implement logic to choose pdf file

--- app/src/main/java/swati4star/createpdf/fragment/PdfToImageFragment.java
+++ app/src/main/java/swati4star/createpdf/fragment/PdfToImageFragment.java
@@ -1,30 +1,146 @@
 package swati4star.createpdf.fragment;
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.dd.morphingbutton.MorphingButton;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import swati4star.createpdf.R;
+import swati4star.createpdf.adapter.MergeFilesAdapter;
+import swati4star.createpdf.interfaces.BottomSheetPopulate;
+import swati4star.createpdf.util.BottomSheetCallback;
+import swati4star.createpdf.util.BottomSheetUtils;
+import swati4star.createpdf.util.FileUtils;
+import swati4star.createpdf.util.MorphButtonUtility;
+import swati4star.createpdf.util.ViewFilesDividerItemDecoration;
+
+import static android.app.Activity.RESULT_OK;
+import static swati4star.createpdf.util.FileUriUtils.getFilePath;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PdfToImageFragment extends Fragment {
+public class PdfToImageFragment extends Fragment implements BottomSheetPopulate, MergeFilesAdapter.OnClickListener {
 
+    private static final int INTENT_REQUEST_PICKFILE_CODE = 10;
+    private Activity mActivity;
+    private String mPath;
+    private MorphButtonUtility mMorphButtonUtility;
+    private FileUtils mFileUtils;
+    private BottomSheetUtils mBottomSheetUtils;
 
-    public PdfToImageFragment() {
-        // Required empty public constructor
-    }
-
+    @BindView(R.id.lottie_progress)
+    LottieAnimationView mLottieProgress;
+    BottomSheetBehavior mSheetBehavior;
+    @BindView(R.id.bottom_sheet)
+    LinearLayout mLayoutBottomSheet;
+    @BindView(R.id.upArrow)
+    ImageView mUpArrow;
+    @BindView(R.id.selectFile)
+    MorphingButton mSelectFileButton;
+    @BindView(R.id.createImages)
+    MorphingButton mCreateImagesButton;
+    @BindView(R.id.created_images)
+    RecyclerView mCreatedImages;
+    @BindView(R.id.pdfToImagesText)
+    TextView mCreateImagesSuccessText;
+    @BindView(R.id.options)
+    LinearLayout options;
+    @BindView(R.id.layout)
+    RelativeLayout mLayout;
+    @BindView(R.id.recyclerViewFiles)
+    RecyclerView mRecyclerViewFiles;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pdf_to_image, container, false);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mActivity = (Activity) context;
+        mMorphButtonUtility = new MorphButtonUtility(mActivity);
+        mFileUtils = new FileUtils(mActivity);
+        mBottomSheetUtils = new BottomSheetUtils(mActivity);
     }
 
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_pdf_to_image, container, false);
+        ButterKnife.bind(this, rootView);
+        mSheetBehavior = BottomSheetBehavior.from(mLayoutBottomSheet);
+        mSheetBehavior.setBottomSheetCallback(new BottomSheetCallback(mUpArrow, isAdded()));
+        mLottieProgress.setVisibility(View.VISIBLE);
+        mBottomSheetUtils.populateBottomSheetWithPDFs(this);
+        resetView();
+        return rootView;
+    }
+
+    private void resetView() {
+        mPath = null;
+        mMorphButtonUtility.initializeButton(mSelectFileButton, mCreateImagesButton);
+    }
+
+    @Override
+    public void onPopulate(ArrayList<String> paths) {
+        if (paths == null || paths.size() == 0) {
+            mLayout.setVisibility(View.GONE);
+        } else {
+            // Init recycler view
+            mRecyclerViewFiles.setVisibility(View.VISIBLE);
+            MergeFilesAdapter mergeFilesAdapter = new MergeFilesAdapter(mActivity,
+                    paths, this);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(mActivity);
+            mRecyclerViewFiles.setLayoutManager(mLayoutManager);
+            mRecyclerViewFiles.setAdapter(mergeFilesAdapter);
+            mRecyclerViewFiles.addItemDecoration(new ViewFilesDividerItemDecoration(mActivity));
+        }
+        mLottieProgress.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.selectFile)
+    public void showFileChooser() {
+        startActivityForResult(mFileUtils.getFileChooser(),
+                INTENT_REQUEST_PICKFILE_CODE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) throws NullPointerException {
+        if (data == null || resultCode != RESULT_OK || data.getData() == null)
+            return;
+        if (requestCode == INTENT_REQUEST_PICKFILE_CODE)
+            setTextAndActivateButtons(getFilePath(data.getData()));
+    }
+
+    @Override
+    public void onItemClick(String path) {
+        mSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        setTextAndActivateButtons(path);
+    }
+
+    private void setTextAndActivateButtons(String path) {
+        mCreatedImages.setVisibility(View.GONE);
+        options.setVisibility(View.GONE);
+        mCreateImagesSuccessText.setVisibility(View.GONE);
+        mPath = path;
+        mMorphButtonUtility.setTextAndActivateButtons(path,
+                mSelectFileButton, mCreateImagesButton);
+    }
 }
