620f27ea9e4e6e0c98bb26559fceb8fa37b567c1
Shivam <shivam.dhuria07@gmail.com>
2018-10-29 23:54:36 +0530

    Fragment creation

--- app/src/main/java/swati4star/createpdf/fragment/RemoveDuplicatePagesFragment.java
+++ app/src/main/java/swati4star/createpdf/fragment/RemoveDuplicatePagesFragment.java
@@ -1,13 +1,137 @@
 package swati4star.createpdf.fragment;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class RemoveDuplicatePagesFragment extends Fragment implements View.OnClickListener {
+import com.airbnb.lottie.LottieAnimationView;
+import com.dd.morphingbutton.MorphingButton;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import swati4star.createpdf.R;
+import swati4star.createpdf.adapter.FilesListAdapter;
+import swati4star.createpdf.adapter.MergeFilesAdapter;
+import swati4star.createpdf.interfaces.BottomSheetPopulate;
+import swati4star.createpdf.util.BottomSheetCallback;
+import swati4star.createpdf.util.BottomSheetUtils;
+import swati4star.createpdf.util.FileUtils;
+import swati4star.createpdf.util.MorphButtonUtility;
+import swati4star.createpdf.util.PDFUtils;
+import swati4star.createpdf.util.ViewFilesDividerItemDecoration;
+
+public class RemoveDuplicatePagesFragment extends Fragment implements MergeFilesAdapter.OnClickListener,
+        FilesListAdapter.OnFileItemClickedListener, BottomSheetPopulate  {
+
+    private Activity mActivity;
+    private String mPath;
+    private MorphButtonUtility mMorphButtonUtility;
+    private FileUtils mFileUtils;
+    private PDFUtils mPDFUtils;
+    private BottomSheetUtils mBottomSheetUtils;
+    private static final int INTENT_REQUEST_PICKFILE_CODE = 10;
+
+    @BindView(R.id.lottie_progress)
+    LottieAnimationView mLottieProgress;
+    @BindView(R.id.selectFile)
+    MorphingButton selectFileButton;
+    //TODO id name
+    @BindView(R.id.Remove)
+    MorphingButton removeDuplicateButton;
+    BottomSheetBehavior sheetBehavior;
+    @BindView(R.id.bottom_sheet)
+    LinearLayout layoutBottomSheet;
+    @BindView(R.id.upArrow)
+    ImageView mUpArrow;
+    @BindView(R.id.downArrow)
+    ImageView mDownArrow;
+    @BindView(R.id.layout)
+    RelativeLayout mLayout;
+    @BindView(R.id.recyclerViewFiles)
+    RecyclerView mRecyclerViewFiles;
+    @BindView(R.id.splitted_files)
+    RecyclerView mSplittedFiles;
+    @BindView(R.id.splitfiles_text)
+    TextView splitFilesSuccessText;
+
     @Override
-    public void onClick(View view) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_remove_duplicate_pages, container, false);
+        ButterKnife.bind(this, rootview);
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+        sheetBehavior.setBottomSheetCallback(new BottomSheetCallback(mUpArrow, isAdded()));
+        mLottieProgress.setVisibility(View.VISIBLE);
+        mBottomSheetUtils.populateBottomSheetWithPDFs(this);
+
+        resetValues();
+        return rootview;
     }
 
 
+
+    private void resetValues() {
+        mPath = null;
+        mMorphButtonUtility.initializeButton(selectFileButton, removeDuplicateButton);
+    }
+
+    private void setTextAndActivateButtons(String path) {
+        mPath = path;
+       // mCompressionInfoText.setVisibility(View.GONE);
+      //  mMorphButtonUtility.setTextAndActivateButtons(path,
+        //        selectFileButton, createPdf);
+    }
+
+    @Override
+    public void onPopulate(ArrayList<String> paths) {
+        if (paths == null || paths.size() == 0) {
+            mLayout.setVisibility(View.GONE);
+        } else {
+            // Init recycler view
+            mRecyclerViewFiles.setVisibility(View.VISIBLE);
+            MergeFilesAdapter mergeFilesAdapter = new MergeFilesAdapter(mActivity,
+                    paths, (MergeFilesAdapter.OnClickListener) this);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(mActivity);
+            mRecyclerViewFiles.setLayoutManager(mLayoutManager);
+            mRecyclerViewFiles.setAdapter(mergeFilesAdapter);
+            mRecyclerViewFiles.addItemDecoration(new ViewFilesDividerItemDecoration(mActivity));
+        }
+        mLottieProgress.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mActivity = (Activity) context;
+        mMorphButtonUtility = new MorphButtonUtility(mActivity);
+        mFileUtils = new FileUtils(mActivity);
+        mPDFUtils = new PDFUtils(mActivity);
+        mBottomSheetUtils = new BottomSheetUtils(mActivity);
+    }
+
+    @Override
+    public void onItemClick(String path) {
+        sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        setTextAndActivateButtons(path);
+    }
+
+    @Override
+    public void onFileItemClick(String path) {
+        mFileUtils.openFile(path);
+    }
 }
 
