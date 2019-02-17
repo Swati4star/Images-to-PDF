db7fb3069a95dc5d2fd373a2baec9b9d205680c1
Shivamdhuria <shivam.dhuria07@gmail.com>
2019-02-17 20:05:09 +0530

    #512 - add invert pdf feature (#560)

---
+++ app/src/main/java/swati4star/createpdf/fragment/InvertPdfFragment.java
@@ -0,0 +1,195 @@
+package swati4star.createpdf.fragment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.airbnb.lottie.LottieAnimationView;
+import com.dd.morphingbutton.MorphingButton;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import swati4star.createpdf.R;
+import swati4star.createpdf.adapter.FilesListAdapter;
+import swati4star.createpdf.adapter.MergeFilesAdapter;
+import swati4star.createpdf.database.DatabaseHelper;
+import swati4star.createpdf.interfaces.BottomSheetPopulate;
+import swati4star.createpdf.interfaces.OnPDFCreatedInterface;
+import swati4star.createpdf.util.BottomSheetCallback;
+import swati4star.createpdf.util.BottomSheetUtils;
+import swati4star.createpdf.util.FileUtils;
+import swati4star.createpdf.util.InvertPdf;
+import swati4star.createpdf.util.MorphButtonUtility;
+import swati4star.createpdf.util.RemoveDuplicates;
+import swati4star.createpdf.util.ViewFilesDividerItemDecoration;
+
+import static android.app.Activity.RESULT_OK;
+import static swati4star.createpdf.util.DialogUtils.createAnimationDialog;
+import static swati4star.createpdf.util.FileUriUtils.getFilePath;
+import static swati4star.createpdf.util.StringUtils.getSnackbarwithAction;
+import static swati4star.createpdf.util.StringUtils.showSnackbar;
+
+public class InvertPdfFragment extends Fragment implements MergeFilesAdapter.OnClickListener,
+        FilesListAdapter.OnFileItemClickedListener, BottomSheetPopulate, OnPDFCreatedInterface {
+
+    private Activity mActivity;
+    private String mPath;
+    private MorphButtonUtility mMorphButtonUtility;
+    private FileUtils mFileUtils;
+    private BottomSheetUtils mBottomSheetUtils;
+    private static final int INTENT_REQUEST_PICKFILE_CODE = 10;
+    private MaterialDialog mMaterialDialog;
+
+    @BindView(R.id.lottie_progress)
+    LottieAnimationView mLottieProgress;
+    @BindView(R.id.selectFile)
+    MorphingButton selectFileButton;
+    @BindView(R.id.invert)
+    MorphingButton invertPdfButton;
+    BottomSheetBehavior sheetBehavior;
+    @BindView(R.id.bottom_sheet)
+    LinearLayout layoutBottomSheet;
+    @BindView(R.id.upArrow)
+    ImageView mUpArrow;
+    @BindView(R.id.downArrow)
+    ImageView mDownArrow;
+    @BindView(R.id.layout)
+    RelativeLayout mLayout;
+    @BindView(R.id.recyclerViewFiles)
+    RecyclerView mRecyclerViewFiles;
+    @BindView(R.id.view_pdf)
+    Button mViewPdf;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_invert_pdf, container, false);
+        ButterKnife.bind(this, rootview);
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+        sheetBehavior.setBottomSheetCallback(new BottomSheetCallback(mUpArrow, isAdded()));
+        mLottieProgress.setVisibility(View.VISIBLE);
+        mBottomSheetUtils.populateBottomSheetWithPDFs(this);
+
+        resetValues();
+        return rootview;
+    }
+    @OnClick(R.id.viewFiles)
+    void onViewFilesClick(View view) {
+        mBottomSheetUtils.showHideSheet(sheetBehavior);
+    }
+
+    /**
+     * Displays file chooser intent
+     */
+    @OnClick(R.id.selectFile)
+    public void showFileChooser() {
+        startActivityForResult(mFileUtils.getFileChooser(),
+                INTENT_REQUEST_PICKFILE_CODE);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) throws NullPointerException {
+        if (data == null || resultCode != RESULT_OK || data.getData() == null)
+            return;
+        if (requestCode == INTENT_REQUEST_PICKFILE_CODE)
+            setTextAndActivateButtons(getFilePath(data.getData()));
+    }
+
+
+//    Inverts colors in PDF
+    @OnClick(R.id.invert)
+    public void parse() {
+        new InvertPdf(mPath, this).execute();
+    }
+
+
+    private void resetValues() {
+        mPath = null;
+        mMorphButtonUtility.initializeButton(selectFileButton, invertPdfButton);
+    }
+
+    private void setTextAndActivateButtons(String path) {
+        mPath = path;
+        mMorphButtonUtility.setTextAndActivateButtons(path,
+                selectFileButton, invertPdfButton);
+    }
+
+    @Override
+    public void onPopulate(ArrayList<String> paths) {
+        if (paths == null || paths.size() == 0) {
+            mLayout.setVisibility(View.GONE);
+        } else {
+            // Init recycler view
+            mRecyclerViewFiles.setVisibility(View.VISIBLE);
+            MergeFilesAdapter mergeFilesAdapter = new MergeFilesAdapter(mActivity, paths, false, this);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(mActivity);
+            mRecyclerViewFiles.setLayoutManager(mLayoutManager);
+            mRecyclerViewFiles.setAdapter(mergeFilesAdapter);
+            mRecyclerViewFiles.addItemDecoration(new ViewFilesDividerItemDecoration(mActivity));
+        }
+        mLottieProgress.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mActivity = (Activity) context;
+        mMorphButtonUtility = new MorphButtonUtility(mActivity);
+        mFileUtils = new FileUtils(mActivity);
+        mBottomSheetUtils = new BottomSheetUtils(mActivity);
+    }
+
+    @Override
+    public void onItemClick(String path) {
+        sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        setTextAndActivateButtons(path);
+    }
+
+    @Override
+    public void onFileItemClick(String path) {
+        mFileUtils.openFile(path);
+    }
+
+    private void viewPdfButton(String path) {
+        mViewPdf.setVisibility(View.VISIBLE);
+        mViewPdf.setOnClickListener(v -> mFileUtils.openFile(path));
+    }
+
+    @Override
+    public void onPDFCreationStarted() {
+        mMaterialDialog = createAnimationDialog(mActivity);
+        mMaterialDialog.show();
+    }
+
+    @Override
+    public void onPDFCreated(boolean isNewPdfCreated, String path) {
+        mMaterialDialog.dismiss();
+        if (!isNewPdfCreated) {
+            showSnackbar(mActivity, R.string.snackbar_invert_unsuccessful);
+            //Hiding View PDF button
+            mViewPdf.setVisibility(View.GONE);
+            return;
+        }
+        new DatabaseHelper(mActivity).insertRecord(path, mActivity.getString(R.string.snackbar_invert_unsuccessful));
+        getSnackbarwithAction(mActivity, R.string.snackbar_pdfCreated)
+                .setAction(R.string.snackbar_viewAction, v -> mFileUtils.openFile(path)).show();
+        viewPdfButton(path);
+        resetValues();
+    }
+}
+
