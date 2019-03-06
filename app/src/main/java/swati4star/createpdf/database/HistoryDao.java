249a5c370124161c1a21c5b8d9d2ba39d0b3fe81
Nitin Verma <canvas.nv@gmail.com>
2019-03-06 23:19:36 +0530

    #588 - add filter in history (#596)

--- app/src/main/java/swati4star/createpdf/database/HistoryDao.java
+++ app/src/main/java/swati4star/createpdf/database/HistoryDao.java
@@ -16,4 +16,7 @@ public interface HistoryDao {
 
     @Query("Delete from History")
     void deleteHistory();
+
+    @Query("select * from history where operation_type IN(:types) order by mId desc")
+    List<History> getHistoryByOperationType(String[] types);
 }
