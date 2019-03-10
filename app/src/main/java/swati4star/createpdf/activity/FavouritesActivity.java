1f8c465dda13ae2d7361eb0fcd1e748851b3c1dd
Jaideep Prasad <jddeep003@gmail.com>
2019-03-10 17:49:05 +0530

    #567 - Added Favourites section (#607)

---
+++ app/src/main/java/swati4star/createpdf/activity/FavouritesActivity.java
@@ -0,0 +1,16 @@
+package swati4star.createpdf.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import swati4star.createpdf.R;
+
+public class FavouritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fav_pref_screen);
+    }
+}
