733ae9e70af2b6d1dfcefec65e2ac997ba5ae495
jd003walker <41433222+jd003walker@users.noreply.github.com>
2019-03-11 21:48:34 +0530

    Added more tests

--- app/src/test/java/swati4star/createpdf/util/ImageUtilsTest.java
+++ app/src/test/java/swati4star/createpdf/util/ImageUtilsTest.java
@@ -11,7 +11,27 @@ public class ImageUtilsTest {
         float testWidth = 8.0f;
         float testHeight = 12.0f;
         Rectangle testDocumentSize = new Rectangle(5.0f, 5.0f, 5.0f, 5.0f);
-
         new ImageUtils().calculateFitSize(testWidth, testHeight, testDocumentSize);
+
+        float testWidth_two = 8.0f;
+        float testHeight_two = 12.0f;
+        Rectangle testDocumentSize_two = new Rectangle(-5.0f, 5.0f, -5.0f, 5.0f);
+        new ImageUtils().calculateFitSize(testWidth_two, testHeight_two, testDocumentSize_two);
+
+        float neg_width = -8.0f;
+        float neg_height = -12.0f;
+        Rectangle testDocumentSize_three = new Rectangle(5.0f, 5.0f, 5.0f, 5.0f);
+        new ImageUtils().calculateFitSize(neg_width, neg_height, testDocumentSize_three);
+
+        float testWidth_three = 8.0f;
+        float testHeight_three = 12.0f;
+        Rectangle testDocumentSize_point = new Rectangle(0.0f, 0.0f, 0.0f, 0.0f);
+        new ImageUtils().calculateFitSize(testWidth_three, testHeight_three, testDocumentSize_point);
+
+        float testWidth_four = 8.0f;
+        float testHeight_four = 12.0f;
+        Rectangle testDocumentSize_line = new Rectangle(5.0f, 5.0f, 0.0f, 0.0f);
+        new ImageUtils().calculateFitSize(testWidth_four, testHeight_four, testDocumentSize_line);
+
     }
 }
