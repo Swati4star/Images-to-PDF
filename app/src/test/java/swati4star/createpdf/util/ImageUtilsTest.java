ccc073a44f6668e1a3f585f13170a1ac58041f31
jd003walker <41433222+jd003walker@users.noreply.github.com>
2019-03-11 22:07:44 +0530

    Made required changes

--- app/src/test/java/swati4star/createpdf/util/ImageUtilsTest.java
+++ app/src/test/java/swati4star/createpdf/util/ImageUtilsTest.java
@@ -13,25 +13,25 @@ public class ImageUtilsTest {
         Rectangle testDocumentSize = new Rectangle(5.0f, 5.0f, 5.0f, 5.0f);
         new ImageUtils().calculateFitSize(testWidth, testHeight, testDocumentSize);
 
-        float testWidth_two = 8.0f;
-        float testHeight_two = 12.0f;
-        Rectangle testDocumentSize_two = new Rectangle(-5.0f, 5.0f, -5.0f, 5.0f);
-        new ImageUtils().calculateFitSize(testWidth_two, testHeight_two, testDocumentSize_two);
+        float testWidthTwo = 8.0f;
+        float testHeightTwo = 12.0f;
+        Rectangle testDocumentSizeTwo = new Rectangle(-5.0f, 5.0f, -5.0f, 5.0f);
+        new ImageUtils().calculateFitSize(testWidthTwo, testHeightTwo, testDocumentSizeTwo);
 
-        float neg_width = -8.0f;
-        float neg_height = -12.0f;
-        Rectangle testDocumentSize_three = new Rectangle(5.0f, 5.0f, 5.0f, 5.0f);
-        new ImageUtils().calculateFitSize(neg_width, neg_height, testDocumentSize_three);
+        float negWidth = -8.0f;
+        float negHeight = -12.0f;
+        Rectangle testDocumentSizeThree = new Rectangle(5.0f, 5.0f, 5.0f, 5.0f);
+        new ImageUtils().calculateFitSize(negWidth, negHeight, testDocumentSizeThree);
 
-        float testWidth_three = 8.0f;
-        float testHeight_three = 12.0f;
-        Rectangle testDocumentSize_point = new Rectangle(0.0f, 0.0f, 0.0f, 0.0f);
-        new ImageUtils().calculateFitSize(testWidth_three, testHeight_three, testDocumentSize_point);
+        float testWidthThree = 8.0f;
+        float testHeightThree = 12.0f;
+        Rectangle testDocumentSizePoint = new Rectangle(0.0f, 0.0f, 0.0f, 0.0f);
+        new ImageUtils().calculateFitSize(testWidthThree, testHeightThree, testDocumentSizePoint);
 
-        float testWidth_four = 8.0f;
-        float testHeight_four = 12.0f;
-        Rectangle testDocumentSize_line = new Rectangle(5.0f, 5.0f, 0.0f, 0.0f);
-        new ImageUtils().calculateFitSize(testWidth_four, testHeight_four, testDocumentSize_line);
+        float testWidthFour = 8.0f;
+        float testHeightFour = 12.0f;
+        Rectangle testDocumentSizeLine = new Rectangle(5.0f, 5.0f, 0.0f, 0.0f);
+        new ImageUtils().calculateFitSize(testWidthFour, testHeightFour, testDocumentSizeLine);
 
     }
 }
