1da294a4126270d869fc72db8a962c8f09121d4f
jd003walker <41433222+jd003walker@users.noreply.github.com>
2019-03-13 20:17:41 +0530

    Updated tests

--- app/src/test/java/swati4star/createpdf/util/ImageUtilsTest.java
+++ app/src/test/java/swati4star/createpdf/util/ImageUtilsTest.java
@@ -4,40 +4,43 @@ import com.itextpdf.text.Rectangle;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ImageUtilsTest {
     private static final Rectangle NO_NORMAL_ERROR =
-            new Rectangle(5.0f, 5.0f, 5.0f, 5.0f);
-    private static final Rectangle NO_INVALID_ERROR =
-            new Rectangle(-5.0f, 5.0f, -5.0f, 5.0f);
-    private static final Rectangle NO_NEGATIVE_LENGTH_ERROR =
-            new Rectangle(5.0f, 5.0f, 5.0f, 5.0f);
+            new Rectangle(0.0f, 0.0f, 0.0f, 0.0f);
     private static final Rectangle NO_POINT_ERROR =
             new Rectangle(0.0f, 0.0f, 0.0f, 0.0f);
     private static final Rectangle NO_LINE_ERROR =
-            new Rectangle(5.0f, 5.0f, 0.0f, 0.0f);
+            new Rectangle(0.0f, 0.0f, 0.0f, 0.0f);
 
     @Test
     public void testcalculateFitSize() {
         float testWidth = 8.0f;
         float testHeight = 12.0f;
         Rectangle testDocumentSize = new Rectangle(5.0f, 5.0f, 5.0f, 5.0f);
-        assertThat(ImageUtils.calculateFitSize(testWidth, testHeight, testDocumentSize),
-                is(NO_NORMAL_ERROR));
+        assertEquals(ImageUtils.calculateFitSize(testWidth, testHeight,
+                testDocumentSize).getLeft(), NO_NORMAL_ERROR.getLeft());
+        assertEquals(ImageUtils.calculateFitSize(testWidth, testHeight,
+                testDocumentSize).getRight(), NO_NORMAL_ERROR.getRight());
 
         float testWidthTwo = 8.0f;
         float testHeightTwo = 12.0f;
         Rectangle testDocumentSizeTwo = new Rectangle(-5.0f, 5.0f, -5.0f, 5.0f);
-        assertThat(ImageUtils.calculateFitSize(testWidthTwo, testHeightTwo, testDocumentSizeTwo),
-                is(NO_INVALID_ERROR));
+        assertEquals(ImageUtils.calculateFitSize(testWidthTwo, testHeightTwo,
+                testDocumentSizeTwo).getLeft(), NO_NORMAL_ERROR.getLeft());
+        assertEquals(ImageUtils.calculateFitSize(testWidthTwo, testHeightTwo,
+                testDocumentSizeTwo).getRight(), NO_NORMAL_ERROR.getRight());
 
         float negWidth = -8.0f;
         float negHeight = -12.0f;
         Rectangle testDocumentSizeThree = new Rectangle(5.0f, 5.0f, 5.0f, 5.0f);
-        assertThat(ImageUtils.calculateFitSize(negWidth, negHeight, testDocumentSizeThree),
-                is(NO_NEGATIVE_LENGTH_ERROR));
+        assertEquals(ImageUtils.calculateFitSize(negWidth, negHeight,
+                testDocumentSizeThree).getLeft(), NO_NORMAL_ERROR.getLeft());
+        assertEquals(ImageUtils.calculateFitSize(negWidth, negHeight,
+                testDocumentSizeThree).getRight(), NO_NORMAL_ERROR.getRight());
 
         float testWidthThree = 8.0f;
         float testHeightThree = 12.0f;
@@ -47,9 +50,11 @@ public class ImageUtilsTest {
 
         float testWidthFour = 8.0f;
         float testHeightFour = 12.0f;
-        Rectangle testDocumentSizeLine = new Rectangle(5.0f, 5.0f, 0.0f, 0.0f);
-        assertThat(ImageUtils.calculateFitSize(testWidthFour, testHeightFour, testDocumentSizeLine),
-                is(NO_LINE_ERROR));
+        Rectangle testDocumentSizeLine = new Rectangle(0.0f, 0.0f, 0.0f, 0.0f);
+        assertEquals(ImageUtils.calculateFitSize(testWidthFour, testHeightFour,
+                testDocumentSizeLine).getLeft(), NO_LINE_ERROR.getLeft());
+        assertEquals(ImageUtils.calculateFitSize(testWidthFour, testHeightFour,
+                testDocumentSizeLine).getRight(), NO_LINE_ERROR.getRight());
 
     }
 }
