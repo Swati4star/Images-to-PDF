2a2abb9b1567b56515772788302a7855d306d44d
jd003walker <41433222+jd003walker@users.noreply.github.com>
2019-03-23 17:24:18 +0530

    Added drive-cloud support

---
+++ app/src/main/java/swati4star/createpdf/DriveServiceHelper.java
@@ -0,0 +1,71 @@
+package swati4star.createpdf;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.OpenableColumns;
+import android.support.v4.util.Pair;
+import com.google.api.services.drive.Drive;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class DriveServiceHelper {
+    private final Executor mExecutor = Executors.newSingleThreadExecutor();
+    private final Drive mDriveService;
+
+    public DriveServiceHelper(Drive googleDriveService) {
+        mDriveService = googleDriveService;
+    }
+    /**
+     * Returns an {@link Intent} for opening the Storage Access Framework file picker.
+     */
+    public Intent createFilePickerIntent() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/plain");
+
+        return intent;
+    }
+
+    /**
+     * Opens the file at the {@code uri} returned by a Storage Access Framework {@link Intent}
+     * created by {@link #createFilePickerIntent()} using the given {@code contentResolver}.
+     */
+    public Task<Pair<String, String>> openFileUsingStorageAccessFramework(
+            ContentResolver contentResolver, Uri uri) {
+        return Tasks.call(mExecutor, () -> {
+            // Retrieve the document's display name from its metadata.
+            String name;
+            try (Cursor cursor = contentResolver.query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    name = cursor.getString(nameIndex);
+                } else {
+                    throw new IOException("Empty cursor returned for file.");
+                }
+            }
+
+            // Read the document's contents as a String.
+            String content;
+            try (InputStream is = contentResolver.openInputStream(uri);
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                content = stringBuilder.toString();
+            }
+
+            return Pair.create(name, content);
+        });
+    }
+}
