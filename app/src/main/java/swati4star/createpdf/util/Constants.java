f8be5cf9af3f89d5d2446d2d6e4d31caff0aa212
jddeep <jddeep003@gmail.com>
2019-04-14 11:35:27 +0530

    Commented new methods

--- app/src/main/java/swati4star/createpdf/util/Constants.java
+++ app/src/main/java/swati4star/createpdf/util/Constants.java
@@ -59,6 +59,7 @@ public class Constants {
     public static final String REORDER_PAGES_KEY = "Reorder Pages";
     public static final String EXTRACT_IMAGES_KEY = "Extract Images";
     public static final String PDF_TO_IMAGES_KEY = "PDF to Images";
+    public static final String PPT_TO_PDF_KEY = "PPT to PDF";
 
     public static final String BUNDLE_DATA = "bundle_data";
     public static final String REORDER_PAGES = "reorder_pages";
