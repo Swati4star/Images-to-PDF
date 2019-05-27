056b0f400f70429168cdab6d62b1da6158e550bb
Jaideep Prasad <jddeep003@gmail.com>
2019-05-27 19:50:42 +0530

    removed Log

--- app/src/main/java/swati4star/createpdf/fragment/AddTextFragment.java
+++ app/src/main/java/swati4star/createpdf/fragment/AddTextFragment.java
@@ -13,7 +13,6 @@ import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
