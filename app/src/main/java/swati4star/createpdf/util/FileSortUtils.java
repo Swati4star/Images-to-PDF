3dfe764ae99636628d70caf6b655e11718f5a068
adrian <30812031+Ashiroq@users.noreply.github.com>
2019-10-31 04:27:10 +0100

    Added tests for FileSortUtils. Changed ArrayList<> to List<> in FileSortUtils

--- app/src/main/java/swati4star/createpdf/util/FileSortUtils.java
+++ app/src/main/java/swati4star/createpdf/util/FileSortUtils.java
@@ -1,8 +1,8 @@
 package swati4star.createpdf.util;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class FileSortUtils {
 
@@ -14,7 +14,7 @@ public class FileSortUtils {
 
     private FileSortUtils(){}
 
-    public void performSortOperation(int option, ArrayList<File> pdf) {
+    public void performSortOperation(int option, List<File> pdf) {
         switch (option) {
             case DATE_INDEX:
                 sortFilesByDateNewestToOldest(pdf);
@@ -38,7 +38,7 @@ public class FileSortUtils {
      *
      * @param filesList list of files to be sorted
      */
-    private void sortByNameAlphabetical(ArrayList<File> filesList) {
+    private void sortByNameAlphabetical(List<File> filesList) {
         Collections.sort(filesList);
     }
 
@@ -47,7 +47,7 @@ public class FileSortUtils {
      *
      * @param filesList list of files to be sorted
      */
-    private void sortFilesByDateNewestToOldest(ArrayList<File> filesList) {
+    private void sortFilesByDateNewestToOldest(List<File> filesList) {
         Collections.sort(filesList, (file, file2) -> Long.compare(file2.lastModified(), file.lastModified()));
     }
 
@@ -56,7 +56,7 @@ public class FileSortUtils {
      *
      * @param filesList list of files to be sorted
      */
-    private void sortFilesBySizeIncreasingOrder(ArrayList<File> filesList) {
+    private void sortFilesBySizeIncreasingOrder(List<File> filesList) {
         Collections.sort(filesList, (file1, file2) -> Long.compare(file1.length(), file2.length()));
     }
 
@@ -65,7 +65,7 @@ public class FileSortUtils {
      *
      * @param filesList list of files to be sorted
      */
-    private void sortFilesBySizeDecreasingOrder(ArrayList<File> filesList) {
+    private void sortFilesBySizeDecreasingOrder(List<File> filesList) {
         Collections.sort(filesList, (file1, file2) -> Long.compare(file2.length(), file1.length()));
     }
 
