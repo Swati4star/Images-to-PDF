7f678a0f891b07eb5912cd333e75369a0e82c65a
Sunny Jain <sunnyjain.sj8@gmail.com>
2020-02-10 13:52:40 +0530

    #788 - Recently Used Feature Enhancement (#885)

---
+++ app/src/main/java/swati4star/createpdf/adapter/RecentListAdapter.java
@@ -0,0 +1,72 @@
+package swati4star.createpdf.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import java.util.List;
+import java.util.Map;
+import swati4star.createpdf.R;
+
+public class RecentListAdapter extends RecyclerView.Adapter<RecentListAdapter.RecentItemViewHolder>  {
+
+    private List<String> mKeys;
+    private List<Map<String, String>> mValues;
+    private final View.OnClickListener mOnClickListener;
+
+    public RecentListAdapter(View.OnClickListener listener) {
+        this.mOnClickListener = listener;
+    }
+
+
+    /**
+     * Updates the recent list
+     * @param keys - list of all the feature viewId
+     * @param recentList - list of the features
+     * */
+    public void updateList(List<String> keys, List<Map<String, String>> recentList) {
+        this.mKeys = keys;
+        this.mValues = recentList;
+    }
+
+    @NonNull @Override public RecentItemViewHolder onCreateViewHolder(
+            @NonNull final ViewGroup viewGroup, final int i) {
+        View mView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_view_enhancement_option, viewGroup, false);
+        return new RecentListAdapter.RecentItemViewHolder(mView);
+    }
+
+    @Override public void onBindViewHolder(
+            @NonNull final RecentItemViewHolder recentItemViewHolder, final int i) {
+
+        recentItemViewHolder.name.setText(recentItemViewHolder.itemView.getContext().getString(
+                Integer.valueOf(mValues.get(i).keySet().iterator().next())
+        ));
+        recentItemViewHolder.icon.setImageDrawable(recentItemViewHolder.itemView.getContext()
+                .getDrawable(Integer.valueOf(mValues.get(i).values().iterator().next())
+        ));
+
+        recentItemViewHolder.itemView.setId(Integer.valueOf(mKeys.get(i)));
+        recentItemViewHolder.itemView.setOnClickListener(mOnClickListener);
+    }
+
+    @Override public int getItemCount() {
+        return mValues.size();
+    }
+
+    class RecentItemViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.option_image) ImageView icon;
+        @BindView(R.id.option_name) TextView name;
+
+
+        private RecentItemViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
--- app/src/main/java/swati4star/createpdf/util/CommonCodeUtils.java
+++ app/src/main/java/swati4star/createpdf/util/CommonCodeUtils.java
@@ -96,52 +96,98 @@ public class CommonCodeUtils {
 
         mFragmentPositionMap = new HashMap<>();
         mFragmentPositionMap.put(homePageItems ? R.id.images_to_pdf : R.id.images_to_pdf_fav,
-                new HomePageItem(R.id.nav_camera, R.string.images_to_pdf));
-        mFragmentPositionMap.put(homePageItems ? R.id.qr_barcode_to_pdf : R.id.qr_barcode_to_pdf_fav,
-                new HomePageItem(R.id.nav_qrcode, R.string.qr_barcode_pdf));
+                new HomePageItem(R.id.nav_camera,
+                        R.drawable.ic_menu_camera,  R.string.images_to_pdf));
+
+        mFragmentPositionMap
+                .put(homePageItems ? R.id.qr_barcode_to_pdf : R.id.qr_barcode_to_pdf_fav,
+                new HomePageItem(R.id.nav_qrcode,
+                        R.drawable.ic_qrcode_24dp, R.string.qr_barcode_pdf));
+
         mFragmentPositionMap.put(homePageItems ? R.id.excel_to_pdf : R.id.excel_to_pdf_fav,
-                new HomePageItem(R.id.nav_excel_to_pdf, R.string.excel_to_pdf));
+                new HomePageItem(R.id.nav_excel_to_pdf,
+                        R.drawable.ic_excel, R.string.excel_to_pdf));
+
         mFragmentPositionMap.put(homePageItems ? R.id.view_files : R.id.view_files_fav,
-                new HomePageItem(R.id.nav_gallery, R.string.viewFiles));
+                new HomePageItem(R.id.nav_gallery,
+                        R.drawable.ic_menu_gallery, R.string.viewFiles));
+
         mFragmentPositionMap.put(homePageItems ? R.id.rotate_pages : R.id.rotate_pages_fav,
-                new HomePageItem(R.id.nav_gallery, R.string.rotate_pages));
+                new HomePageItem(R.id.nav_gallery,
+                        R.drawable.baseline_crop_rotate_24, R.string.rotate_pages));
+
         mFragmentPositionMap.put(homePageItems ? R.id.extract_text : R.id.extract_text_fav,
-                new HomePageItem(R.id.nav_text_extract, R.string.extract_text));
+                new HomePageItem(R.id.nav_text_extract,
+                        R.drawable.ic_broken_image_black_24dp, R.string.extract_text));
+
         mFragmentPositionMap.put(homePageItems ? R.id.add_watermark : R.id.add_watermark_fav,
-                new HomePageItem(R.id.nav_add_watermark, R.string.add_watermark));
+                new HomePageItem(R.id.nav_add_watermark,
+                        R.drawable.ic_branding_watermark_black_24dp, R.string.add_watermark));
+
         mFragmentPositionMap.put(homePageItems ? R.id.merge_pdf : R.id.merge_pdf_fav,
-                new HomePageItem(R.id.nav_merge, R.string.merge_pdf));
+                new HomePageItem(R.id.nav_merge,
+                        R.drawable.ic_merge_type_black_24dp, R.string.merge_pdf));
+
         mFragmentPositionMap.put(homePageItems ? R.id.split_pdf : R.id.split_pdf_fav,
-                new HomePageItem(R.id.nav_split, R.string.split_pdf));
+                new HomePageItem(R.id.nav_split,
+                        R.drawable.ic_call_split_black_24dp, R.string.split_pdf));
+
         mFragmentPositionMap.put(homePageItems ? R.id.text_to_pdf : R.id.text_to_pdf_fav,
-                new HomePageItem(R.id.nav_text_to_pdf, R.string.text_to_pdf));
+                new HomePageItem(R.id.nav_text_to_pdf,
+                        R.drawable.ic_text_format_black_24dp, R.string.text_to_pdf));
+
         mFragmentPositionMap.put(homePageItems ? R.id.compress_pdf : R.id.compress_pdf_fav,
-                new HomePageItem(R.id.nav_compress_pdf, R.string.compress_pdf));
+                new HomePageItem(R.id.nav_compress_pdf,
+                        R.drawable.ic_compress_image, R.string.compress_pdf));
+
         mFragmentPositionMap.put(homePageItems ? R.id.remove_pages : R.id.remove_pages_fav,
-                new HomePageItem(R.id.nav_remove_pages, R.string.remove_pages));
+                new HomePageItem(R.id.nav_remove_pages,
+                        R.drawable.ic_remove_circle_black_24dp, R.string.remove_pages));
+
         mFragmentPositionMap.put(homePageItems ? R.id.rearrange_pages : R.id.rearrange_pages_fav,
-                new HomePageItem(R.id.nav_rearrange_pages, R.string.reorder_pages));
+                new HomePageItem(R.id.nav_rearrange_pages,
+                        R.drawable.ic_rearrange, R.string.reorder_pages));
+
         mFragmentPositionMap.put(homePageItems ? R.id.extract_images : R.id.extract_images_fav,
-                new HomePageItem(R.id.nav_extract_images, R.string.extract_images));
+                new HomePageItem(R.id.nav_extract_images,
+                        R.drawable.ic_broken_image_black_24dp, R.string.extract_images));
+
         mFragmentPositionMap.put(homePageItems ? R.id.view_history : R.id.view_history_fav,
-                new HomePageItem(R.id.nav_history, R.string.history));
+                new HomePageItem(R.id.nav_history,
+                        R.drawable.ic_history_black_24dp, R.string.history));
+
         mFragmentPositionMap.put(homePageItems ? R.id.pdf_to_images : R.id.pdf_to_images_fav,
-                new HomePageItem(R.id.nav_pdf_to_images, R.string.pdf_to_images));
+                new HomePageItem(R.id.nav_pdf_to_images,
+                        R.drawable.ic_image_black_24dp, R.string.pdf_to_images));
+
         mFragmentPositionMap.put(homePageItems ? R.id.add_password : R.id.add_password_fav,
-                new HomePageItem(R.id.nav_add_password, R.string.add_password));
+                new HomePageItem(R.id.nav_add_password,
+                        R.drawable.ic_lock_black_24dp, R.string.add_password));
+
         mFragmentPositionMap.put(homePageItems ? R.id.remove_password : R.id.remove_password_fav,
-                new HomePageItem(R.id.nav_remove_password, R.string.remove_password));
+                new HomePageItem(R.id.nav_remove_password,
+                        R.drawable.ic_lock_open_black_24dp, R.string.remove_password));
+
         mFragmentPositionMap.put(homePageItems ? R.id.add_images : R.id.add_images_fav,
-                new HomePageItem(R.id.nav_add_images, R.string.add_images));
+                new HomePageItem(R.id.nav_add_images,
+                        R.drawable.ic_add_black_24dp, R.string.add_images));
+
         mFragmentPositionMap.put(homePageItems ?
                         R.id.remove_duplicates_pages_pdf : R.id.remove_duplicates_pages_pdf_fav,
-                new HomePageItem(R.id.nav_remove_duplicate_pages, R.string.remove_duplicate_pages));
+                new HomePageItem(R.id.nav_remove_duplicate_pages,
+                        R.drawable.ic_remove_duplicate_square_black, R.string.remove_duplicate_pages));
+
         mFragmentPositionMap.put(homePageItems ? R.id.invert_pdf : R.id.invert_pdf_fav,
-                new HomePageItem(R.id.nav_invert_pdf, R.string.invert_pdf));
+                new HomePageItem(R.id.nav_invert_pdf,
+                        R.drawable.ic_invert_color_24dp, R.string.invert_pdf));
+
         mFragmentPositionMap.put(homePageItems ? R.id.zip_to_pdf : R.id.zip_to_pdf_fav,
-                new HomePageItem(R.id.nav_zip_to_pdf, R.string.zip_to_pdf));
+                new HomePageItem(R.id.nav_zip_to_pdf,
+                        R.drawable.ic_zip_to_pdf, R.string.zip_to_pdf));
+
         mFragmentPositionMap.put(homePageItems ? R.id.add_text : R.id.add_text_fav,
-                new HomePageItem(R.id.nav_add_text, R.string.add_text));
+                new HomePageItem(R.id.nav_add_text,
+                        R.drawable.ic_text_format_black_24dp, R.string.add_text));
 
         return mFragmentPositionMap;
 
