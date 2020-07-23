bf2d16bc0e11141c734395d7ea38f2a1aefb9924
Raj Datta Manohar <rajdattasunny@gmail.com>
2020-07-24 00:09:03 +0530

    Fixed Bug where Merge PDF Button would throw error

--- app/src/main/java/swati4star/createpdf/util/StringUtils.java
+++ app/src/main/java/swati4star/createpdf/util/StringUtils.java
@@ -8,8 +8,6 @@ import android.view.inputmethod.InputMethodManager;
 
 import java.util.Objects;
 
-import static swati4star.createpdf.util.Constants.pdfDirectory;
-
 /**
  * Created by anandparmar on 18/06/18.
  */
@@ -68,8 +66,7 @@ public class StringUtils {
     }
 
     public String getDefaultStorageLocation() {
-        return Environment.getExternalStorageDirectory().getAbsolutePath() +
-                pdfDirectory;
+        return Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
     }
 
     /**
