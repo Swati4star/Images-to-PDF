066d65079d393df9640f69f5bf30b6684ff48898
Haripriya B <haripriya.baskaran@thoughtworks.com>
2020-10-20 23:42:06 +0530

    #829 - Added unit test for StringUtils (#967)

---
+++ app/src/androidTest/java/swati4star/createpdf/util/StringUtilsTest.java
@@ -0,0 +1,38 @@
+package swati4star.createpdf.util;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class StringUtilsTest {
+    StringUtils stringUtils;
+
+    @Before
+    public void setup() {
+        stringUtils = StringUtils.getInstance();
+    }
+
+    @Test
+    public void isEmptyTest() {
+        Assert.assertTrue(stringUtils.isEmpty(""));
+    }
+
+    @Test
+    public void isNotEmptyTest() {
+        Assert.assertTrue(stringUtils.isNotEmpty("Not empty"));
+    }
+
+    @Test
+    public void getDefaultStorageLocationTest() {
+        Assert.assertNotNull(stringUtils.getDefaultStorageLocation());
+    }
+
+    @Test
+    public void parseIntOrDefaultTest() {
+        Assert.assertEquals(stringUtils.parseIntOrDefault("25", 0), 25);
+    }
+}
