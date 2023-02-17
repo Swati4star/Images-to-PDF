2f857bb70d1f46e451425cba42200cb563724a33
Gaurav Saini <codegsaini@gmail.com>
2023-02-17 18:05:44 +0530

    Update PermissionsUtils.java
    - Removed redundant method

--- app/src/main/java/swati4star/createpdf/util/PermissionsUtils.java
+++ app/src/main/java/swati4star/createpdf/util/PermissionsUtils.java
@@ -2,24 +2,15 @@ package swati4star.createpdf.util;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
-import android.provider.Settings;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
 
-import static swati4star.createpdf.util.Constants.READ_PERMISSIONS;
-import static swati4star.createpdf.util.Constants.REQUEST_CODE_FOR_WRITE_PERMISSION;
-import static swati4star.createpdf.util.Constants.WRITE_PERMISSIONS;
-
-import swati4star.createpdf.R;
 
 /**
  * !! IMPORTANT !!
@@ -105,47 +96,7 @@ public class PermissionsUtils {
         if (requestCode == expectedRequest && grantResults.length > 0) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 whenSuccessful.run();
-            } else {
-                showPermissionDenyDialog(context, requestCode);
             }
-        }
-    }
-
-    private void showPermissionDenyDialog(Activity activity, int requestCode) {
-        String[] permission;
-        if (requestCode == REQUEST_CODE_FOR_WRITE_PERMISSION) {
-            permission = WRITE_PERMISSIONS;
-        } else {
-            permission = READ_PERMISSIONS;
-        }
-        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission[0])) {
-            new AlertDialog.Builder(activity)
-                    .setTitle(R.string.permission_denied_text)
-                    .setMessage(R.string.storage_need_rationale_description)
-                    .setPositiveButton(R.string.ask_again_text, (dialog, which) -> {
-                        requestRuntimePermissions(activity, permission, REQUEST_CODE_FOR_WRITE_PERMISSION);
-                        dialog.dismiss();
-                    })
-                    .setNegativeButton(R.string.cancel_text, (dialog, which) -> {
-                        dialog.dismiss();
-                    }).show();
-        } else if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, permission[0])) {
-            new AlertDialog.Builder(activity)
-                    .setTitle(R.string.permission_denied_text)
-                    .setMessage(R.string.storage_need_rationale_for_not_ask_again_flag)
-                    .setPositiveButton(R.string.enable_from_settings_text, (dialog, which) -> {
-                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                        Uri uri = Uri.fromParts("package", activity.getPackageName(), null);
-                        intent.setData(uri);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-                        activity.startActivity(intent);
-                        dialog.dismiss();
-                    })
-                    .setNegativeButton(R.string.cancel_text, (dialog, which) -> {
-                        dialog.dismiss();
-                    }).show();
         }
     }
 }
